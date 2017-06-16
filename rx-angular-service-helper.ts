8f3615a497e620b28e064afb908946a03771dd69
Nicholas Lowman <screenm0nkey@users.noreply.github.com>
2017-06-16 12:30:02 +0100

    Update rx-angular-service-helper.ts

--- rx-angular-service-helper.ts
+++ rx-angular-service-helper.ts
@@ -1,83 +1,73 @@
 /*@MODULE*/
-class SplitterBarController implements  angular.IComponentController {
-	static $inject = [ '$scope', '$element', '$document', 'spgRx' ];
+import Observable = Rx.Observable;
+const DraggableDirective: angular.IDirectiveFactory = ($document: angular.IDocumentService, rx: SpgRxService, draggableService: SpgDraggableService) => ({
+    restrict: 'A',
+    require: '^^spgDraggableContainer',
+    link(scope: angular.IScope, element: JQuery, attrs: Draggable.Attrs, ctrl: DraggableContainerController) {
+        const {left: initX, top: initY} = element.css(['left', 'top']);
+        const [ elHeight, elWidth ] = [element.height(), element.width()];
+        let draggableSubscription;
 
-	/*BINDINGS*/
-	notifier: SpgRx.ISubject<Splitter.Position>;
-	isVertical: boolean;
-	index: number;
-	onDragBegin: () => void;
-	onDragMove: (params: {coords: Splitter.Coord, index: number}) => number;
-	onDragComplete: () => void;
+        element.attr('draggable', 'false');
+        scope.$on('$destroy', () => draggableSubscription && draggableSubscription.dispose());
 
-	domInputs: SpgRx.ISubscription<{x: number, y: number}>;
-	max: boolean;
+        draggableSubscription = rx
+            .fromEvent(element, 'mousedown')
+            .map(draggableService.getCoorinates)
+            .flatMapLatest(initateMousedown)
+            .do(onDragStart)
+            .flatMapLatest(onMouseMove)
+            .map(draggableService.getCoorinates)
+            .throttle(40)
+            .do(onDrag)
+            .subscribe();
 
-	private pSizeBounds: [number, number];
+        /*
+         Initiates the mousedown action as soon as the mouse moves 10px from it's original location.
+         The coordinates from here used to calculate the distance the mouse has moved.
+         The stream completes as soon as one set of coordinates have been emitted.
+         The merge(rx.timer) initiates the mousedown action if the user clicks but doesn't move the mouse.
+         TakeUntil is needed in case there is a mousedown and mouseup without moving the mouse.
+         */
+        function initateMousedown(mousedownCoords: Draggable.Coord): SpgRx.IObservable<Draggable.Coord> {
+            return rx
+                .fromEvent($document, 'mousemove')
+                .map(draggableService.getCoorinates)
+                .filter(mousemoveCoords => draggableService.getDistanceBetween(mousemoveCoords, mousedownCoords) > 10)
+                .throttle(25)
+                .merge(rx.timer(500).map(() => mousedownCoords))
+                .takeUntil(rx.fromEvent($document, 'mouseup'))
+                .take(1)
+        }
 
-	constructor(
-	  private $scope: angular.IScope,
-	  private $element: angular.IRootElementService,
-	  private $document: angular.IDocumentService,
-	  private rx: SpgRxService
-	) {}
+        /*
+         Emits the coordinates of the mouse whilst it's in a mousedown state.
+         Completes on mouseup action.
+         */
+        function onMouseMove(): SpgRx.IObservable<Draggable.Coord> {
+            return rx
+                .fromEvent($document, 'mousemove')
+                .takeUntil(rx.fromEvent($document, 'mouseup'))
+                .finally(onDragEnd)
+        }
 
-	get sizeBounds(): [number, number] {
-		return this.pSizeBounds || (this.pSizeBounds = this.isVertical ? [2, 86] : [1, 98]);
-	}
+        function onDragStart({x, y}:  Draggable.Coord) {
+            element.addClass('drag-active');
+            ctrl.onDragStart(element);
+            onDrag({x, y});
+        }
 
-	$postLink() {
-		//Subscribe to the 'mousedown' event
-		this.domInputs = this.rx.fromEvent(this.$element, 'mousedown')
-		  //We dont want multiple clicks within short timeframe to interrupt
-		  .throttle(25)
-		  //Notify parent that a drag has started
-		  .do(() => this.onDragBegin())
-		  //Switch subscription to the 'mousemove' event
-		  .flatMapLatest(() => this.rx
-		    .fromEvent(this.$document, 'mousemove')
-		      //Unsubscribe if there is a 'mouseup' event or if there is a position notification from the container
-		      .takeUntil(this.rx.using(
-		        () => ({dispose: () => this.onDragComplete()}),
-		        () => this.rx.merge(this.rx.fromEvent(this.$document, 'mouseup'), this.notifier.asObservable().filter(val => !!val))
-		      ))
-		  )
-		  //Throttle by 15ms
-		  .sample(15)
-		  //Get the current x and y position
-		  .map(({ pageX, pageY }) => ({x: pageX, y: pageY}))
-		  //Notify the parent
-		  .subscribe(coords => {
-			  let pctSize = this.onDragMove({coords, index: this.index});
-			  let [ min, max ] = this.sizeBounds;
-			  this.max = pctSize > max || pctSize < min;
-		  });
-	}
+        function onDrag({x, y}: Draggable.Coord) {
+            let {x: left, y: top} = draggableService.getOffsetPosition({x, y}, {height: elHeight, width: elWidth});
+            element.css({left, top});
+            ctrl.onDrag({x, y});
+        }
 
-	$onDestroy() {
-		this.domInputs && this.domInputs.dispose();
-	}
-}
+        function onDragEnd() {
+            element.removeClass('drag-active').css({left: initX || 0, top: initY || 0});
+            ctrl.onDragEnd(element, attrs.spgDraggable, scope.$eval(attrs.draggablePayload));
+        }
+    }
+});
 
-const SplitterBarComponent: angular.IComponentOptions = {
-	controller: SplitterBarController,
-	bindings: {
-		notifier: '<',
-		isVertical: '<',
-		index: '<',
-		onDragBegin: '&',
-		onDragMove: '&',
-		onDragComplete: '&'
-	},
-	template:
-		`<div class="splitter" ng-class="$ctrl.isVertical ? 'vertical' : 'horizontal'">
-			<span class="resize-arrow resize-up-left" ng-class="{ max: vm.max || vm.isLeft }">
-				<i class="grip"></i>
-			</span>
-		</div>`
-};
-
-angular.module('munisSalesLink').component('splitterBarComponent', SplitterBarComponent);
-
-
-
+angular.module('munisSalesLink').directive('spgDraggable', ['$document', 'spgRx', 'spgDraggableService', DraggableDirective]);
